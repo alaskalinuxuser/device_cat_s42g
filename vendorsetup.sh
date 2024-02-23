91a471f54f1b88b38256a3d70c06c64e58cd4532
lopestorm <guillopes2002@gmail.com>
2024-02-23 12:33:17 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_S42G-user
-add_lunch_combo twrp_S42G-userdebug
-add_lunch_combo twrp_S42G-eng
+#add_lunch_combo twrp_S42G-user
+#add_lunch_combo twrp_S42G-userdebug
+#add_lunch_combo twrp_S42G-eng
